44ae3006824e2eedf27e63bde819abf0349a3c24
Sumanth <sumanth30805@gmail.com>
2026-06-26 12:02:00 +0530

    feat(search): define SearchFieldProps interface in types

--- src/components/ui/search/SearchField.types.ts
+++ src/components/ui/search/SearchField.types.ts
@@ -1,2 +1,7 @@
+import { InputHTMLAttributes } from "react";
 export type SearchFieldSize = 'compact' | 'default';
 export type SearchFieldFont = 'mono' | 'body';
+export interface SearchFieldProps extends Omit<InputHTMLAttributes<HTMLInputElement>, "size" | "onChange"> {
+  value: string;
+  onChange: (value: string) => void;
+}
